8f6f9582549d56c8e1f7378c3299cbd8506dae29
91902 <ramajs2003@gmail.com>
2021-07-24 21:06:46 +0530

    A date quiz is done Vibrations and colour change Landscape variation and the states are stored timer is set and penalities are given accordingly penalty to final score high score is stored using shared preferences

--- app/src/main/java/com/example/task1/viewclass.java
+++ app/src/main/java/com/example/task1/viewclass.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class viewclass extends ViewModel {
     int score=0,k=1,c=-1,m;
-    int mint=1,s=30,resec=30,wrng=0;
+    int wrng=0;
     public int vgetscore()
     {
         return score;
@@ -19,13 +19,6 @@ public class viewclass extends ViewModel {
    public int vgetwrng(){return wrng;}
     public void addwrng(){ wrng=wrng+1;
     }
-    public void subtime(){
-        if(s-10>0&&s-10<=59)
-            s=s-10;
-        else{
-            s=s-10+59;
-            mint--;
-        }  }
     public int getk()
     {
         return k;
@@ -44,27 +37,5 @@ public class viewclass extends ViewModel {
          m = new Random().nextInt(12)+1;
         return m;
     }
-    public int vgetmint(){return mint;}
-    public int vgetsec(){
-        return s;
-
-    }
-    public void submint(){mint=mint-1;}
-    public void subsecond(){
-        if(s>0&&s<=59)
-           s=s-1;
-        else
-            s=59;}
-
-    public void addsec(){
-        s=s+1;
-    }
-    public void resetscore(){
-        score=0;
-    }
-    /*public void resettime(){
-        mint=1;
-        s=20;
-    }*/
 
 }
